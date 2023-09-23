bd46401965f83057786126bad4ec86595b980132
David Cuellar <davidcuellarh@icloud.com>
2023-09-22 22:11:27 -0500

    Clase final con ejercicio solicitado

--- Platzino/Platzino/Program.cs
+++ Platzino/Platzino/Program.cs
@@ -1,70 +1,122 @@
 ﻿using System.Drawing;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 int totalJugador = 0;
 int totalDealer = 0;
 int num = 0;
+int platziCoins = 0;
 string message = "";
 string controlOtraCarta = "";
 string switchControl = "menu";
+int userBet = 0;
+
 
 System.Random random = new System.Random();
 
 //Blackjack, Juntar 21 pidiendo, en casa de pasarte de 21 pierdes.
 //cartas o en caso de tener menos
 //de 21 igual tener mayor puntuación que el dealer
+
 while (true)
 {
-    totalJugador = 0;
-    totalDealer = 0;
 
-    switch (switchControl)
+
+    Console.WriteLine("Welcome al P l a t z i n o");
+    Console.WriteLine("¿Desea Jugar? \n Escriba ‘Si’ para jugar o ‘No’ para salir ");
+    string deseaJugar = Console.ReadLine();
+    if (deseaJugar == "s" || deseaJugar == "Si" || deseaJugar == "si")
     {
-        case "menu":
-            Console.WriteLine("Welcome al c a s i n o");
-            Console.WriteLine("Escriba ‘21’ para jugar al 21");
-            switchControl = Console.ReadLine();
-            break;
-        case "21":
-            do
-            {
-                num = random.Next(1, 12);
-                totalJugador = totalJugador + num;
-                Console.WriteLine("Toma tu carta, jugador,");
-                Console.WriteLine($"Te salió el número: {num} ");
-                Console.WriteLine("¿Deseas otra carta ?");
-                controlOtraCarta = Console.ReadLine();
+        Console.WriteLine("¡Comencemos!");
+        Console.WriteLine("¿Cuántos PlatziCoins deseas? \n" +
+                       "Ingresa un número entero \n" +
+                       "Recuerda que necesitas 1 por ronda");
+        platziCoins = int.Parse(Console.ReadLine());
 
-            } while (controlOtraCarta == "Si" ||
-        controlOtraCarta == "si"
-        || controlOtraCarta == "yes");
+    }
+    else
+    {
+        Console.WriteLine("¡Hasta luego!");
+        Environment.Exit(0);
+    }
+       
 
-            totalDealer = random.Next(14, 23);
-            Console.WriteLine($"El dealer tiene {totalDealer}");
+    for (int i = 0; i < platziCoins; i++)
+    {
+        totalJugador = 0;
+        totalDealer = 0;
 
-            if (totalJugador > totalDealer && totalJugador < 22)
-            {
-                message = "Venciste al dealer, felicidades";
-                switchControl = "menu";
-            }
-            else if (totalJugador >= 22)
-            {
-                message = "Perdiste vs el dealer, te pasaste de 21";
-                switchControl = "menu";
-            }
-            else if (totalJugador <= totalDealer)
-            {
-                message = "Perdiste vs el dealer, lo siento";
-                switchControl = "menu";
-            }
-            else
-            {
-                message = "condición no válida";
-            }
-            Console.WriteLine(message);
-            break;
-        default:
-            Console.WriteLine("Valor ingresa no válido en el  C A S I N O");
-            break;
+        switch (switchControl)
+        {
+            case "menu":
+                Console.WriteLine($"Tienes {platziCoins} y has gastado {i}");
+                Console.WriteLine("Escriba ‘21’ para jugar al 21");
+                switchControl = Console.ReadLine();
+
+                if (userBet == 0)
+                {
+                    Console.WriteLine("¿Cuántos PlatziCoins deseas comprar? Ingresa un número entero: ");
+                    userBet = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Has comprado {userBet} PlatziCoins para apostar");
+                }
+                else
+                {
+                    Console.WriteLine($"actualmente tienes {userBet} PlatziCoins ");
+                    Console.WriteLine("¿Cuántos PlatziCoins deseas comprar? \n" +
+                                                      "Ingresa un número entero \n");
+                    userBet = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Has apostado {userBet} PlatziCoins para apostar");
+                }
+                i = i - 1;
+                break;
+            case "21":
+                do
+                {
+                    num = random.Next(1, 12);
+                    totalJugador = totalJugador + num;
+                    Console.WriteLine("Toma tu carta, jugador,");
+                    Console.WriteLine($"Te salió el número: {num} ");
+                    Console.WriteLine("¿Deseas otra carta ?");
+                    controlOtraCarta = Console.ReadLine();
+
+                } while (controlOtraCarta == "Si" ||
+                         controlOtraCarta == "si" ||
+                         controlOtraCarta == "yes");
+
+                totalDealer = random.Next(14, 23);
+                Console.WriteLine($"El dealer tiene {totalDealer}");
+
+                if (totalJugador > totalDealer && totalJugador < 22)
+                {
+                    message = "Venciste al dealer, felicidades";
+                    platziCoins = platziCoins + 1; // aumenta el número de platziCoins si gana el usuario
+                    Console.Clear();//limpia la consola
+                    userBet = userBet + userBet; // aumenta el número de platziCoins si gana el usuario
+                    switchControl = "menu";
+
+                }
+                else if (totalJugador >= 22)
+                {
+                    message = "Perdiste vs el dealer, te pasaste de 21";
+                    userBet = userBet - userBet;
+                    switchControl = "menu";
+                }
+                else if (totalJugador <= totalDealer)
+                {
+                    message = "Perdiste vs el dealer, lo siento";
+                    userBet = userBet - userBet;
+                    switchControl = "menu";
+                }
+                else
+                {
+                    message = "condición no válida";
+                }
+                Console.WriteLine(message);
+                break;
+            default:
+                Console.WriteLine("Valor ingresa no válido en el  C A S I N O");
+                Environment.Exit(0);
+                
+                break;
+        }
     }
 }
